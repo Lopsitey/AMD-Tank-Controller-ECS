d62fa55e16faa4e1d4294fd6dd0fb10f41304478
Jake Astles <jakeastles13@gmail.com>
2026-01-26 15:16:34 +0000

    Finished Basic ECS

--- Assets/ECS/Authoring/EnemySpawnerAuthoring.cs
+++ Assets/ECS/Authoring/EnemySpawnerAuthoring.cs
@@ -11,12 +11,13 @@ namespace ECS.Authoring
 
         private class EnemySpawnerBaker : Baker<EnemySpawnerAuthoring>
         {
+            //An entity acts as an ID associated with individual components that contain data about the entity.
+            //Unlike GameObjects, entities contain no code: they're units of data that the systems you create process.
             public override void Bake(EnemySpawnerAuthoring authoring)
             {
                 // Gets the spawner entity itself (primary entity)
                 // If this was TransformUsageFlags.None the spawner would be static and wouldn't move
                 Entity spawnerEntity = GetEntity(TransformUsageFlags.Dynamic);
-                
                 // Creates a fresh spawner component to add to the spawner entity 
                 // This just fills out the component data - nothing spawned yet
                 AddComponent(spawnerEntity, new EnemySpawnerComponent
@@ -26,7 +27,9 @@ namespace ECS.Authoring
                     entityToSpawn = GetEntity(authoring.m_Prefab, TransformUsageFlags.Dynamic),
                     spawnDelay = authoring.m_SpawnDelay,
                     spawnPos = authoring.transform.position,
-                    timer = 0.0f
+                    timer = 0.0f,
+                    name = authoring.name,
+                    spawnCount = 0
                 });
                 Debug.Log($"Baking {authoring.name} to {spawnerEntity.Index}");
             }
