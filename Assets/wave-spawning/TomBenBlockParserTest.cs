ed7b0be941e945dee216efceec603bd92c05ec30
Jake Astles <jakeastles13@gmail.com>
2026-02-09 20:59:48 +0000

    Finished The Parser However It Still Only Outputs To The Console Next steps are to improve it and populate the arrays in the WaveSpawnerDataAuthoring class with a Scripted Importer. Getting it to spawn actual objects in the WaveSpawnerSystem.cs would also be good.

--- Assets/wave-spawning/TomBenBlockParserTest.cs
+++ Assets/wave-spawning/TomBenBlockParserTest.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using ECS.wave_spawning;
 using UnityEngine;
 
 #endregion
@@ -73,7 +75,7 @@ namespace wave_spawning
         /// <param name="text">The text to be trimmed.</param>
         /// <param name="end">The amount of characters to be removed from the text.</param>
         /// <returns>A substring with the last end.Length characters removed.</returns>
-        private string TrimFromEnd(string text, string end) => text[0..^end.Length];
+        private string TrimFromEnd(string text, string end) => text[..^end.Length];
         // The "0.." syntax starts at the beginning of the string.
         // ^end.Length means “end.Length characters from the end”.
         // Could also be done with text.Substring(0, text.Length - end.Length)
@@ -96,14 +98,14 @@ namespace wave_spawning
         private void HandleInsideHeaderState(char c, ref ParsedBlock tempBlock)
         {
             //If the Tom brace hasn't been hit then you're not at the end of the header yet
-            if(!BufferEndsWith("_Tom")) return;
-            
+            if (!BufferEndsWith("_Tom")) return;
+
             //Otherwise get the entire header, excluding the _Tom at the end
             string header = TrimFromEnd(m_Buffer, "_Tom");
-            
+
             //Set the current block's header to this
             tempBlock.m_Header = header;
-            
+
             //Change to the body state
             ChangeState(BlockParserState.InsideBody);
         }
@@ -117,20 +119,224 @@ namespace wave_spawning
         private bool HandleInsideBodyState(char c, ref ParsedBlock tempBlock)
         {
             //If the Ben brace hasn't been hit then you're not at the end of the body yet
-            if(!BufferEndsWith("_Ben")) return false;
-            
+            if (!BufferEndsWith("_Ben")) return false;
+
             //Otherwise get the entire body, excluding the _ben at the end
             string body = TrimFromEnd(m_Buffer, "_ben");
-            
+
             //Set the current block's body to this
             tempBlock.m_Body = body;
-            
+
             //Change back to the outside block state, ready for the next block
             ChangeState(BlockParserState.OutsideBlock);
-            
+
             return true;
         }
 
+        /// <summary>
+        /// This takes in a block header and parses it into a ParsedBlockHeader object, which contains the type, id and name of the block.
+        /// </summary>
+        /// <param name="block">The block to be parsed</param>
+        /// <returns></returns>
+        /// <exception cref="UnityException">If the header was corrupt or invalid</exception>
+        public ParsedBlockHeader GetBlockHeader(in ParsedBlock block)
+        {
+            //Matches all subgroups using the regex pattern to match the data, for more info go to regexer.com
+            var match = Regex.Match(block.m_Header,
+                @"(type|cluster|wave)\s*\-\s*(\d+)\s*(?:\((.+)\))?");
+
+            //If the match fails then the header is invalid
+            if (!match.Success)
+                throw new UnityException("Invalid block header");
+
+            // Instantiates a ParsedBlockHeader with the relevant data
+            // The type and id are taken from the regex groups
+            return new ParsedBlockHeader
+            {
+                // This stores the entire header text
+                m_RawText = block.m_Header,
+                m_Type = GetBlockTypeFromString(match.Groups[1].Value),
+                m_ID = int.Parse(match.Groups[2].Value),
+                // The name is null if the name doesn't exist as it is optional
+                m_Name = match.Groups[3].Success ? match.Groups[3].Value : null
+            };
+        }
+
+        /// <summary>
+        /// This converts the type string from the header into a ParsedBlockType enum
+        /// </summary>
+        /// <param name="type">The type to be converted</param>
+        /// <returns>The converted type</returns>
+        /// <exception cref="UnityException">An error if the type did not match any of the enum types</exception>
+        public ParsedBlockType GetBlockTypeFromString(string type) => type switch
+        {
+            "wave" => ParsedBlockType.Wave,
+            "cluster" => ParsedBlockType.Cluster,
+            "type" => ParsedBlockType.UnitType,
+            //This is essentially the default case for the switch statement - if the type did not match any of the above then it is invalid
+            _ => throw new UnityException($"Invalid block type {type}")
+        };
+
+        /// <summary>
+        /// This parses the block data into a unit authoring object.
+        /// It does this by using the ID and name from the header and splitting the body to fill out the rest of the data.
+        /// </summary>
+        /// <param name="block">The current block being parsed.</param>
+        /// <param name="header">The header of the block being parsed.</param>
+        /// <returns>The unit with the parsed data applied.</returns>
+        public UnitAuthoring ParseUnitBlock(in ParsedBlock block, in ParsedBlockHeader header)
+        {
+            //Instantiate a new UnitAuthoring to be filled with the parsed data
+            UnitAuthoring unit = new UnitAuthoring
+            {
+                id = header.m_ID,
+                name = header.m_Name
+            };
+
+            // Gets all the rules from the body and splits them by interrobang
+            var rules = block.m_Body.Split("!?");
+
+            // Iterates through every rule, splitting them by the => to get the key and value
+            foreach (var rule in rules)
+            {
+                // If there is no rule after splitting then skip
+                if (rule.Trim().Length <= 0)
+                    continue;
+
+                // Rules are in the key value format so should always have a length of 2
+                string[] splitRule = rule.Trim().Split("=>");
+                Debug.Assert(splitRule.Length == 2);
+
+                // Separates the key and value
+                string key = splitRule[0];
+                string value = splitRule[1];
+
+                // Assigns the relevant values using the key
+                if (key == "damage") unit.damage = float.Parse(value);
+                else if (key == "speed") unit.speed = float.Parse(value);
+                else if (key == "health") unit.health = float.Parse(value);
+                //Errors if the key is weird
+                else Debug.LogWarning($"Unknown key {key} in unit block with id {header.m_ID}");
+            }
+
+            return unit;
+        }
+
+        /// <summary>
+        /// Works essentially the same as the unit block parser but with a for loop to assign values to the cluster array rather than individual values.
+        /// </summary>
+        /// <param name="block">The current block being parsed.</param>
+        /// <param name="header">The header of the block being parsed.</param>
+        /// <returns>The cluster containing the array of parsed rules.</returns>
+        public ClusterAuthoring ParseClusterBlock(in ParsedBlock block, in ParsedBlockHeader header)
+        {
+            var rules = block.m_Body.Split("!?");
+
+            //Could be an array but a list works better in a foreach and means that you don't have to mess with the size before usage
+            List<ClusterRuleAuthoring> validRules = new List<ClusterRuleAuthoring>();
+
+            //Using a for loop here so the index can be used to assign the rules to the correct place in the array in the cluster
+            foreach (var rule in rules)
+            {
+                if (rule.Trim().Length <= 0)
+                    continue;
+
+                string[] splitRule = rule.Trim().Split(":");
+                Debug.Assert(splitRule.Length == 2,
+                    $"Key value pair unexpected length in cluster block with id {header.m_ID}");
+
+                string key = splitRule[0];
+                string value = splitRule[1];
+
+                var newRule = new ClusterRuleAuthoring()
+                {
+                    unitId = int.Parse(key),
+                    amount = int.Parse(value)
+                };
+
+                validRules.Add(newRule);
+            }
+
+            return new ClusterAuthoring
+            {
+                id = header.m_ID,
+                name = header.m_Name,
+                clusterRules = validRules.ToArray()
+            };
+        }
+
+        public WaveAuthoring ParseWaveBlock(in ParsedBlock block, in ParsedBlockHeader header)
+        {
+            WaveAuthoring wave = new WaveAuthoring
+            {
+                id = header.m_ID,
+                name = header.m_Name
+            };
+
+            //Split, Trim, and Filter empty strings
+            string[] ruleStrings = block.m_Body.Split("!?");
+            List<string> validRuleStrings = new List<string>();
+            foreach (var rule in ruleStrings)
+            {
+                var trim = rule.Trim();
+                if (trim.Length <= 0) continue;
+                validRuleStrings.Add(trim);
+            }
+            ruleStrings = validRuleStrings.ToArray();
+            Debug.Assert(ruleStrings.Length <= 0, $"No valid rules found in wave block with ID: {header.m_ID}");
+            
+            wave.waveRules = new WaveRuleAuthoring[ruleStrings.Length];
+
+            // Iterates through individual rules, parsing and storing them in the wave's array of rules
+            for (int i = 0; i < ruleStrings.Length; ++i)
+                wave.waveRules[i] = ParseSingleWaveRule(ruleStrings[i]);
+
+            return wave;
+        }
+
+        /// <summary>
+        /// Compact Regex parser that handles the parsing of a single wave rule.
+        /// It looks for the type and ID at the start of the string, and then looks for the optional timer and population cap.
+        /// </summary>
+        /// <param name="ruleText">The wave to be parsed.</param>
+        /// <returns>The converted data.</returns>
+        private WaveRuleAuthoring ParseSingleWaveRule(string ruleText)
+        {
+            var rule = new WaveRuleAuthoring
+            {
+                triggerPopulationCap = -1,
+                triggerTimeSinceLastSpawn = -1
+            };
+            
+            // If it starts with (used ^) T or C (Group 1), followed by ID digits (used \d) (Group 2)
+            var mainMatch = Regex.Match(ruleText, @"^([TC])(\d+)");
+
+            // Looks for <float> anywhere in the string
+            var timeMatch = Regex.Match(ruleText, @"<(\d+)>");
+
+            // Looks for [int] anywhere in the string
+            var popMatch = Regex.Match(ruleText, @"\[(\d+)\]");
+
+            if (!mainMatch.Success)
+            {
+                Debug.LogWarning($"Invalid wave rule format: {ruleText}");
+                return rule;
+            }
+
+            // Sets the type using char and the ID num from the initial match
+            rule.type = (mainMatch.Groups[1].Value == "T") ? WaveRuleType.Unit : WaveRuleType.Cluster;
+            rule.clusterOrTypeId = int.Parse(mainMatch.Groups[2].Value);
+
+            // Sets the timer if found, otherwise it remains -1
+            if (timeMatch.Success)
+                rule.triggerTimeSinceLastSpawn = float.Parse(timeMatch.Groups[1].Value);
+
+            // Same here for the pop cap
+            if (popMatch.Success)
+                rule.triggerPopulationCap = int.Parse(popMatch.Groups[1].Value);
+
+            return rule;
+        }
 
         /// <summary>
         /// This is a very simple block parser which takes in a string of text and parses it into blocks.
@@ -141,6 +347,9 @@ namespace wave_spawning
         /// <returns></returns>
         public List<ParsedBlock> ParseBlocksFromFile(string fileContent)
         {
+            //Resets the parser state to outside the block and clears the buffer, ready for the new block to be read in 
+            ResetParser();
+
             List<ParsedBlock> parsedBlocks = new List<ParsedBlock>();
             ParsedBlock tempBlock = new ParsedBlock();
             foreach (char c in fileContent)
@@ -164,6 +373,7 @@ namespace wave_spawning
                             //Reset the temp block for the next block to be read in.
                             tempBlock = new ParsedBlock();
                         }
+
                         break;
                 }
             }
@@ -177,8 +387,12 @@ namespace wave_spawning
     /// </summary>
     public class TomBenBlockParserTest : MonoBehaviour
     {
-        [SerializeField] public string m_FilePath;
-        [SerializeField] public List<ParsedBlock> m_ParsedBlocks;
+        [SerializeField] private string m_FilePath;
+        private List<ParsedBlock> m_ParsedBlocks;
+
+        [SerializeField] private List<UnitAuthoring> m_ParsedUnits;
+        [SerializeField] private List<ClusterAuthoring> m_ParsedClusters;
+        [SerializeField] private List<WaveAuthoring> m_ParsedWaves;
 
         private void Awake()
         {
@@ -192,7 +406,39 @@ namespace wave_spawning
             // Creates a new parser and passes in the file text
             TomBenBlockParser parser = new TomBenBlockParser();
             m_ParsedBlocks = parser.ParseBlocksFromFile(fileText);
+
+            foreach (ParsedBlock block in m_ParsedBlocks)
+            {
+                ParsedBlockHeader header = parser.GetBlockHeader(block);
+                //If it's a unit block then parse it as one
+                if (header.m_Type == ParsedBlockType.UnitType)
+                    m_ParsedUnits.Add(parser.ParseUnitBlock(block, header));
+                else if (header.m_Type == ParsedBlockType.Cluster)
+                    m_ParsedClusters.Add(parser.ParseClusterBlock(block, header));
+                else if (header.m_Type == ParsedBlockType.Wave)
+                    m_ParsedWaves.Add(parser.ParseWaveBlock(block, header));
+
+                Debug.Log($"<b>{header.m_RawText}</b>");
+                Debug.Log($"\t Type: {header.m_Type}");
+                Debug.Log($"\t ID: {header.m_ID}");
+                Debug.Log($"\t Name: {header.m_Name}");
+            }
         }
+    }
+
+    public class ParsedBlockHeader
+    {
+        public string m_RawText;
+        public ParsedBlockType m_Type;
+        public int m_ID;
+        public string m_Name;
+    }
+
+    public enum ParsedBlockType
+    {
+        UnitType,
+        Cluster,
+        Wave
     }
 
     public enum BlockParserState
--- Assets/wave-spawning/WaveSpawnerSystem.cs
+++ Assets/wave-spawning/WaveSpawnerSystem.cs
@@ -1,9 +1,11 @@
+
 using ECS.Components;
+using ECS.wave_spawning;
 using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
-namespace ECS.wave_spawning
+namespace wave_spawning
 {
     /// <summary>
     /// The wave spawner system, which uses wave spawner data
@@ -46,41 +48,66 @@ namespace ECS.wave_spawning
             //Relevant data for use in this function
             var waves = waveSpawnerData.waves.GetKeyValueArrays(Allocator.Temp);
             float timeSinceSpawn = GetTimeSinceLastSpawn(ref state);
+
+            // 2. Safety Check: If we've run out of waves entirely, mark finished and exit
+            //If completed all waves, set finished to true to skip logic in future updates
+            if (CompletedWaves(waves))
+            {
+                waveIndex = 0;
+                waveRuleIndex = 0;
+
+                if (!hasFinished)
+                {
+                    Debug.Log("Completed all waves!");
+                    hasFinished = true;
+                }
+
+                // Return early to avoid trying to access wave data that doesn't exist
+                return;
+            }
+
             var currentWaveData = waves.Values[waves.Keys[waveIndex]];
-            
+
+            // If all rules have been completed or the wave was empty, move to the next wave.
+            if (CompletedWaveRules(currentWaveData))
+            {
+                // Move to the next wave
+                waveIndex++;
+                waveRuleIndex = 0; // Reset rule index for the new wave
+
+                return;
+            }
+
             var currentRule = currentWaveData.waveRules[waveRuleIndex];
-            
+
             //Whether a population cap or timer has been specified
             bool specifiedPopCap = currentRule.triggerPopulationCap >= 0;
             bool specifiedTimer = currentRule.triggerTimeSinceLastSpawn >= 0;
-            
+
             //Checks whether either has been exceeded, triggering the next wave rule
             bool metPopThreshold = enemyCount <= currentRule.triggerPopulationCap;
-            bool timerExceeded = timer < currentRule.triggerTimeSinceLastSpawn;
-            
-            if((specifiedPopCap && metPopThreshold) || (specifiedTimer && timerExceeded))
+            bool timerExceeded = timer >= currentRule.triggerTimeSinceLastSpawn;
+
+            if ((specifiedPopCap && metPopThreshold) || (specifiedTimer && timerExceeded))
             {
-                waveRuleIndex++;
-                lastWaveSpawnTime = (float)SystemAPI.Time.ElapsedTime;//Now
+                lastWaveSpawnTime = (float)SystemAPI.Time.ElapsedTime; //Now
                 timer = 0.0f;
 
-                //Gets all units and clusters
-                var units = waveSpawnerData.units;
-                var clusters = waveSpawnerData.clusters;
-            
-                var currentUnit = units[waveRuleIndex];
-                var currentCluster = clusters[waveRuleIndex];
-                
                 if (currentRule.type == WaveRuleType.Cluster)
                 {
+                    // Gets all clusters
+                    var clusters = waveSpawnerData.clusters;
+                    var currentCluster = clusters[currentRule.clusterOrTypeId];
+
                     foreach (var rule in currentCluster.clusterRules)
                     {
-                        Debug.Log($"Spawning {rule.amount} of unit {currentUnit.name} from cluster {currentCluster.name} as part of wave {currentWaveData.name}");
+                        Debug.Log($"Cluster: {currentCluster.name} as part of wave {currentWaveData.name}.");
+                        Debug.Log($"Spawning {rule.amount} units of type {rule.unitId}.");
                         for (int i = 0; i < rule.amount; i++)
                         {
                             Debug.Log($"Spawning unit {i} in the cluster.");
                             //Entity enemy = ecb.Instantiate(currentUnit);
-                                
+
                             //float3 pos = new float3(0f, 0f, 0f);
 
                             //ecb.SetComponent(enemy, LocalTransform.FromPosition(pos));
@@ -89,44 +116,32 @@ namespace ECS.wave_spawning
                 }
                 else if (currentRule.type == WaveRuleType.Unit)
                 {
+                    var units = waveSpawnerData.units;
+                    var currentUnit = units[currentRule.clusterOrTypeId];
+
                     Debug.Log($"Spawning individual unit {currentUnit.name} as part of wave {currentWaveData.name}");
                     //Entity enemy = ecb.Instantiate(currentUnit);
-                                
+
                     //float3 pos = new float3(0f, 0f, 0f);
 
                     //ecb.SetComponent(enemy, LocalTransform.FromPosition(pos));
                 }
-                
+
                 if (timeSinceSpawn < currentRule.triggerTimeSinceLastSpawn)
                     Debug.Log($"Wave rule triggered by timer! Time since last spawn: {timeSinceSpawn}, required time: {currentRule.triggerTimeSinceLastSpawn}");
                 else if (metPopThreshold)
                     Debug.Log($"Wave rule triggered by population cap! Current population: {enemyCount}, required population: {currentRule.triggerPopulationCap}");
-            }
-            
-            //If completed all rules for this wave, move to the next wave
-            if (CompletedWaveRules(currentWaveData))
-            {
-                waveRuleIndex = 0;
-                waveIndex++;
-            }
 
-            //If completed all waves, set finished to true to skip logic in future updates
-            if (CompletedWaves(waves))
-            {
-                waveIndex = 0;
-                waveRuleIndex = 0;
-                hasFinished = true;
-                
-                Debug.Log("Completed all waves!");
+                waveRuleIndex++;
             }
         }
 
         public void OnUpdate(ref SystemState state)
         {
             //Finished all waves? Skip logic
-            if(hasFinished)
+            if (hasFinished)
                 return;
-        
+
             //Grab ECB & wave spawner data
             this.ecb = GetECB(ref state);
             var waveSpawner = SystemAPI.GetSingleton<SpawnerDataSingleton>();
@@ -136,10 +151,10 @@ namespace ECS.wave_spawning
 
             //Increase timer
             timer += SystemAPI.Time.DeltaTime;
-            
+
             enemyCount = GetEntityCount(ref state, enemyQuery);
             Debug.Log($"There are currently {enemyCount} enemies in the world.");
-            
+
             //And dispose of ECB
             this.ecb.Playback(state.EntityManager);
             this.ecb.Dispose();
@@ -150,7 +165,7 @@ namespace ECS.wave_spawning
         private EntityQuery GetEntityCountQuery<T>(ref SystemState state) where T : unmanaged, IComponentData
             => state.GetEntityQuery(typeof(T));
 
-        private int GetEntityCount(ref SystemState state, EntityQuery query) 
+        private int GetEntityCount(ref SystemState state, EntityQuery query)
             => query.CalculateEntityCount();
 
         private float GetTimeSinceLastSpawn(ref SystemState state)
@@ -175,7 +190,6 @@ namespace ECS.wave_spawning
             foreach (var (enemy, entity) in SystemAPI.Query<RefRW<EnemySpawnerComponent>>().WithEntityAccess())
                 ecb.DestroyEntity(entity);
         }
-
 
         #endregion
     }
